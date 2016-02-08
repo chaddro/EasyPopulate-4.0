b966137332c416a16bea160ddc9075c0f7c8cf39
mc12345678 <mc12345678@cox.net>
2016-02-07 23:34:05 -0500

    Break the dependence of products_description.

--- admin/easypopulate_4_import.php
+++ admin/easypopulate_4_import.php
@@ -1883,59 +1883,90 @@ if (!is_null($_POST['import']) && isset($_POST['import'])) {
 
           // BEGIN: Products Descriptions
           // the following is common in both the updating an existing product and creating a new product // mc12345678 updated to allow omission of v_products_description in the import file.
-          if (isset($v_products_name)) {
-            foreach ($v_products_name as $key => $name) {
+    $add_products_description_data = false;
+    $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_ADD_OR_CHANGE_DATA');
+    if (isset($v_products_name) || isset($v_products_description) || ($ep_supported_mods['psd'] == true && isset($v_products_short_desc) ) || isset($v_products_url) || $add_products_description_data) { // 
+      // Effectively need a way to step through all language options, this section to be "accepted" if there is something to be updated.  Prefer the ability to verify update need without having to loop on anything, but just by "presence" of information.
+      foreach ($languages as $lang) {
+        // foreach ($v_products_name as $key => $name) {  // Decouple the dependency on the products_name being imported to update the products_name, description, short description and/or URL. //mc12345678 2015-Dec-12
+        $lang_id = $lang['id'];
               $sql = "SELECT * FROM " . TABLE_PRODUCTS_DESCRIPTION . " WHERE
                         products_id = :products_id: AND
                         language_id = :language_id:";
               $sql = $db->bindVars($sql, ':products_id:', $v_products_id, 'integer');
-              $sql = $db->bindVars($sql, ':language_id:', $key, 'integer');
+        $sql = $db->bindVars($sql, ':language_id:', $lang_id, 'integer');
               $result = ep_4_query($sql);
 
               if (($ep_uses_mysqli ? mysqli_num_rows($result) : mysql_num_rows($result)) == 0) {
                 $sql = "INSERT INTO " . TABLE_PRODUCTS_DESCRIPTION . " (
                             products_id,
-                            language_id,
-                            products_name, " .
-                        ((isset($filelayout['v_products_description_' . $key]) || ( isset($filelayout['v_products_description_' . $key]) && $product_is_new) ) ? " products_description," : "");
-                if ($ep_supported_mods['psd'] == true) {
+                  " . (isset($filelayout['v_products_name_' . $lang_id]) || $product_is_new ? "products_name, " : "") . 
+                  ((isset($filelayout['v_products_description_' . $lang_id]) || ( isset($filelayout['v_products_description_' . $lang_id]) && $product_is_new) ) ? " products_description, " : "");
+          if ($ep_supported_mods['psd'] == true && isset($v_products_short_desc)) {
                   $sql .= " products_short_desc,";
                 }
-                $sql .= " products_url )
+          $sql .= (isset($filelayout['v_products_url_' . $lang_id]) ? " products_url, " : "");
+          $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_INSERT_FIELDS');
+          $sql .= "
+                   language_id )
                             VALUES (
-                            :products_id:,
-                            :language_id:,
-                            :products_name:, " .
-                        ((isset($filelayout['v_products_description_' . $key]) || ( isset($filelayout['v_products_description_' . $key]) && $product_is_new) ) ? " :products_description:," : "");
-                if ($ep_supported_mods['psd'] == true) {
-                  $sql .= " :products_short_desc:,";
+                   :v_products_id:,
+                   " . (isset($filelayout['v_products_name_' . $lang_id]) || $product_is_new ? ":v_products_name:, " : "") .
+                   ((isset($filelayout['v_products_description_' . $lang_id]) || ( isset($filelayout['v_products_description_' . $lang_id]) && $product_is_new) ) ? ":v_products_description:, " : "");
+          if ($ep_supported_mods['psd'] == true && isset($v_products_short_desc)) {
+            $sql .= ":v_products_short_desc:, ";
                 }
-                $sql .= " :products_url:)";
-                $sql = $db->bindVars($sql, ':products_id:', $v_products_id, 'integer');
-                $sql = $db->bindVars($sql, ':language_id:', $key, 'integer');
-                $sql = $db->bindVars($sql, ':products_name:', $name, 'string');
-                $sql = $db->bindVars($sql, ':products_description:', $v_products_description[$key], 'string');
-                $sql = $db->bindVars($sql, ':products_short_desc:', $v_products_short_desc[$key], 'string');
-                $sql = $db->bindVars($sql, ':products_url:', $v_products_url[$key], 'string');
-                $result = ep_4_query($sql);
-                if ($result) {
-                  zen_record_admin_activity('New product ' . (int) $v_products_id . ' description added via EP4.', 'info');
+          $sql .= (isset($filelayout['v_products_url_' . $lang_id]) ? ":v_products_url:, " : "");
+          $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_INSERT_FIELDS_VALUES');
+          $sql .= "
+                   :language_id:)";
+
+          $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_FIELDS_BIND_START');
+          $sql = $db->bindVars($sql, ':v_products_id:', $v_products_id, 'integer');
+          $sql = $db->bindVars($sql, ':language_id:', $lang_id, 'integer');
+          $sql = $db->bindVars($sql, ':v_products_name:', $v_products_name[$lang_id], 'string');
+          $sql = $db->bindVars($sql, ':v_products_description:', $v_products_description[$lang_id], 'string');
+          $sql = $db->bindVars($sql, ':v_products_short_desc:', $v_products_short_desc[$lang_id], 'string');
+          $sql = $db->bindVars($sql, ':v_products_url:', $v_products_url[$lang_id], 'string');
+          $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_FIELDS_BIND_END');
+
+          $result = ep_4_query($sql);
+          if ($result) {
+            zen_record_admin_activity('New product ' . (int)$v_products_id . ' description added via EP4.', 'info');
+          }
+        } else { // already in the description, update it
+          $sql = "UPDATE " . TABLE_PRODUCTS_DESCRIPTION . " SET ";
+          $update_count = false;
+          if (isset($filelayout['v_products_name_' . $lang_id])) {
+            $sql .= " products_name      = :products_name";
+            $update_count = true;
+          }
+          if (isset($filelayout['v_products_description_' . $lang_id]) || ( isset($filelayout['v_products_description_' . $lang_id]) && $product_is_new)) {
+            $sql .= ($update_count ? ", " : "") . "products_description = :v_products_description:";
+            $update_count = true;
+          }
+          if ($ep_supported_mods['psd'] == true && isset($v_products_short_desc[$lang_id])) {
+            $sql .= ($update_count ? ", " : "") . "products_short_desc = :v_products_short_desc:";
                 }
-              } else { // already in the description, update it
-                $sql = "UPDATE " . TABLE_PRODUCTS_DESCRIPTION . " SET
-       products_name        = :products_name:, " .
-                        ((isset($filelayout['v_products_description_' . $key]) || ( isset($filelayout['v_products_description_' . $key]) && $product_is_new) ) ? "products_description = :products_description:," : "");
-                if ($ep_supported_mods['psd'] == true) {
-                  $sql .= " products_short_desc = :products_short_desc:,";
-                }
-                $sql .= " products_url= :products_url:
-       WHERE products_id = :products_id: AND language_id = :language_id:";
-                $sql = $db->bindVars($sql, ':products_name:', $name, 'string');
-                $sql = $db->bindVars($sql, ':products_description:', $v_products_description[$key], 'string');
-                $sql = $db->bindVars($sql, ':products_short_desc:', $v_products_short_desc[$key], 'string');
-                $sql = $db->bindVars($sql, ':products_url:', $v_products_url[$key], 'string');
-                $sql = $db->bindVars($sql, ':products_id:', $v_products_id, 'integer');
-                $sql = $db->bindVars($sql, ':language_id:', $key, 'integer');
+          if (isset($filelayout['v_products_url_' . $lang_id])) {
+            $sql .= ($update_count ? ", " : "") . " products_url = :v_products_url: ";
+            $update_count = true;
+          }
+                
+          $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_UPDATE_FIELDS_VALUES');
+                
+          $sql .= "        WHERE products_id = :v_products_id: AND language_id = :language_id:";
+                
+          $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_UPDATE_WHERE');
+                
+          $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_FIELDS_BIND_START');
+          $sql = $db->bindVars($sql, ':v_products_name:', $v_products_name[$lang_id], 'string');
+          $sql = $db->bindVars($sql, ':v_products_description:', $v_products_description[$lang_id], 'string');
+          $sql = $db->bindVars($sql, ':v_products_short_desc:', $v_products_short_desc[$lang_id], 'string');
+          $sql = $db->bindVars($sql, ':v_products_url:', $v_products_url[$lang_id], 'string');
+          $sql = $db->bindVars($sql, ':v_products_id:', $v_products_id, 'integer');
+          $sql = $db->bindVars($sql, ':language_id:', $lang_id, 'integer');
+          $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_FIELDS_BIND_END');
                 $result = ep_4_query($sql);
                 if ($result) {
                   zen_record_admin_activity('Updated product ' . (int) $v_products_id . ' description via EP4.', 'info');
