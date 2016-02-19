bdde10e44e1e276b7a7038b98581040fc037833a
mc12345678 <mc12345678@users.noreply.github.com>
2016-02-19 11:17:03 -0500

    Correctly loop through languages
    Incorrectly used the variable $languages instead of $langcode at line 1908.  This was not a problem on the system tested because of additionally installed software.  Further the use of $langcode is an EP4 variable and should have been used instead of the standard $languages variable used elsewhere in ZC.  $langcode should have been used originally to stay within the normal structure of EP4 but was overlooked and did not present a problem (testing should have been with a clean install of ZC as well as this module.)

--- admin/easypopulate_4_import.php
+++ admin/easypopulate_4_import.php
@@ -1905,7 +1905,7 @@ if (!is_null($_POST['import']) && isset($_POST['import'])) {
     $zco_notifier->notify('EP4_IMPORT_FILE_PRODUCTS_DESCRIPTION_ADD_OR_CHANGE_DATA');
     if (isset($v_products_name) || isset($v_products_description) || ($ep_supported_mods['psd'] == true && isset($v_products_short_desc) ) || isset($v_products_url) || $add_products_description_data) { // 
       // Effectively need a way to step through all language options, this section to be "accepted" if there is something to be updated.  Prefer the ability to verify update need without having to loop on anything, but just by "presence" of information.
-      foreach ($languages as $lang) {
+      foreach ($langcode as $lang) {
         // foreach ($v_products_name as $key => $name) {  // Decouple the dependency on the products_name being imported to update the products_name, description, short description and/or URL. //mc12345678 2015-Dec-12
         $lang_id = $lang['id'];
               $sql = "SELECT * FROM " . TABLE_PRODUCTS_DESCRIPTION . " WHERE
